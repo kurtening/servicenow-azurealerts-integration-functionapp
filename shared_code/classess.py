f77ff58bd76dc6f47c89b193958a9b9ebde87a80
kurtening <kurtranzelflores@gmail.com>
2022-09-26 22:03:51 +0800

    Initial shared_code commit

---
+++ shared_code/classess.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+import requests
+
+
+@dataclass
+class ServiceNow:
+    """Service Now API Details
+    """
+    # The instance of your service now "dev.service-now.com"
+    instance: str
+    # Service Account Username 
+    # (Must have itil, rest_service, and web services role)
+    username: str
+    # Service Account Password
+    password: str
+    pass
+
+
+@dataclass
+class NewIncident(ServiceNow):
+    # JSON String of the SNOW Fields
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    table: str = "/api/now/table/incident"
+
+    def create_incident(self, body: str) -> dict:
+        url = f"https://{self.instance}{self.table}"
+        response = requests.post(
+            url, auth=(self.username, self.password),
+            headers=self.headers,
+            data=body
+            )
+        # Check for HTTP codes other than 200
+        if response.status_code not in list(range(200, 300)):
+            print('Status:',
+                  response.status_code,
+                  'Headers:', response.headers)
+            return response.json()
+
+        # Decode the JSON response into a dictionary and use the data
+        data = response.json()
+        return data
---
+++ shared_code/constants.py
@@ -0,0 +1,3 @@
+INSTANCE = "dev89153.service-now.com"
+USERNAME = "SVC_Account"
+SECRET = "%3]Z]P&CZ*]xsZR1uJ0ke&]6:$Db]Yq;ZayeCUQOF[%.<CWTi,bs(oOWAp>R_t!1gSl7>!f5sE<R(uX@>}E>:k$^?>1QWIlQ!{n3" # noqa
